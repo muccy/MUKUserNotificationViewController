6914dd6db7c79139b9ff33aacfd9ae796435432e
Muccy <muccymac@gmail.com>
2014-07-25 20:43:04 +0200

    Mocking model object

---
+++ Pod/Classes/MUKUserNotification.h
@@ -0,0 +1,13 @@
+//
+//  MUKUserNotification.h
+//  MUKUserNotificationViewController
+//
+//  Created by Marco on 25/07/14.
+//  Copyright (c) 2014 Muccy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MUKUserNotification : NSObject
+@property (nonatomic, copy) NSString *text;
+@end
---
+++ Pod/Classes/MUKUserNotification.m
@@ -0,0 +1,13 @@
+//
+//  MUKUserNotification.m
+//  MUKUserNotificationViewController
+//
+//  Created by Marco on 25/07/14.
+//  Copyright (c) 2014 Muccy. All rights reserved.
+//
+
+#import "MUKUserNotification.h"
+
+@implementation MUKUserNotification
+
+@end
